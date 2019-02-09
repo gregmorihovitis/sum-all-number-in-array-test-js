79c860b1992b20627ff468d9c8e24e36ce96a438
gregmorihovitis <gregmorihovitis@gmail.com>
2019-02-09 20:40:36 +0000

    completed exercise

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  return array.reduce(function fnctn(a, b) {
+    if (Array.isArray(b)) {
+        return b.reduce(fnctn, a);
+    } 
+    else if (b === Math.floor(b)) {
+        return a + b;
+    } 
+
+    return a;
+}, 0);
 }
 
 module.exports = sumItems;
